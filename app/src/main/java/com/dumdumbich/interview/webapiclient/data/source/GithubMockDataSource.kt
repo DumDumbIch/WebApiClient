a2eab2189400ece95cadfc4a3781b40e02955c40
DumDumbIch <dumdumbich@gmail.com>
2022-03-29 22:10:00 +0300

    Usecases: stub

--- app/src/main/java/com/dumdumbich/interview/webapiclient/data/source/GithubMockDataSource.kt
+++ app/src/main/java/com/dumdumbich/interview/webapiclient/data/source/GithubMockDataSource.kt
@@ -1,95 +1,42 @@
 package com.dumdumbich.interview.webapiclient.data.source
 
-import android.annotation.SuppressLint
 import com.dumdumbich.interview.webapiclient.domain.entity.Repository
 import com.dumdumbich.interview.webapiclient.domain.entity.User
-import com.dumdumbich.interview.webapiclient.domain.usecase.GithubUsecase
-import java.lang.IllegalStateException
-import java.text.SimpleDateFormat
-import java.util.*
+import com.dumdumbich.interview.webapiclient.domain.usecase.DataSourceUsecase
 
 
-class GithubMockDataSource : GithubUsecase {
-
-    private val users: MutableList<User> = emptyList<User>().toMutableList()
-    private val repositories: MutableList<Repository> = emptyList<Repository>().toMutableList()
-
+class GithubMockDataSource : DataSourceUsecase {
 
     override fun getUser(
         login: String,
         onSuccess: (User) -> Unit,
         onError: (Throwable) -> Unit
     ) {
-        if (users.isNotEmpty()) {
-            for (user in users) {
-                if (login == user.login) {
-                    onSuccess(user)
-                }
-            }
-        } else {
-            onError(IllegalStateException("GithubMockDataSource : User by login not found"))
-        }
+        TODO("Not yet implemented")
     }
 
     override fun getUsers(
         onSuccess: (List<User>) -> Unit,
         onError: (Throwable) -> Unit
     ) {
-        onSuccess(createUsers(20))
+        TODO("Not yet implemented")
     }
 
-    override fun getUserRepository(
-        userLogin: String,
+    override fun getRepository(
         repositoryName: String,
+        userLogin: String,
         onSuccess: (Repository) -> Unit,
         onError: (Throwable) -> Unit
     ) {
-        onSuccess(createRepository())
+        TODO("Not yet implemented")
     }
 
-    override fun getUserRepositories(
-        login: String,
+    override fun getRepositories(
+        userLogin: String,
         onSuccess: (List<Repository>) -> Unit,
         onError: (Throwable) -> Unit
     ) {
-        onSuccess(createRepositories(20))
-    }
-
-    private fun createUser(item: Int): User =
-        User(
-            "",
-            "Login $item",
-            "https//github/login$item",
-            Random().nextInt()
-        )
-
-    private fun createUsers(items: Int): List<User> {
-        users.clear()
-        for (item in 1..items) users.add(createUser(item))
-        return users
-    }
-
-    private fun createRepository(): Repository =
-        Repository(
-            "Name ${UUID.randomUUID().toString()}",
-            "DumDumbIch",
-            getFictionDateTime(),
-            Random().nextInt(10),
-            Random().nextInt()
-        )
-
-    private fun createRepositories(items: Int): List<Repository> {
-        repositories.clear()
-        for (item in 1..items) repositories.add(createRepository())
-        return repositories
-    }
-
-    @SuppressLint("SimpleDateFormat")
-    private fun getFictionDateTime(): String {
-        val calendar = GregorianCalendar()
-        calendar.set(Calendar.ERA, GregorianCalendar.AD)
-        val dataFormat = SimpleDateFormat("dd MMM yyy GG")
-        return dataFormat.format(calendar.time)
+        TODO("Not yet implemented")
     }
 
 }
--- app/src/main/java/com/dumdumbich/interview/webapiclient/data/storage/RoomDataStorage.kt
+++ app/src/main/java/com/dumdumbich/interview/webapiclient/data/storage/RoomDataStorage.kt
@@ -1,128 +1,75 @@
 package com.dumdumbich.interview.webapiclient.data.storage
 
 import android.content.Context
-import androidx.room.Room
-import com.dumdumbich.interview.webapiclient.data.db.room.GithubDb
-import com.dumdumbich.interview.webapiclient.data.db.room.repository.RepositoryDao
-import com.dumdumbich.interview.webapiclient.data.db.room.repository.RepositoryEntity
-import com.dumdumbich.interview.webapiclient.data.db.room.user.UserDao
-import com.dumdumbich.interview.webapiclient.data.db.room.user.UserEntity
 import com.dumdumbich.interview.webapiclient.domain.entity.Repository
 import com.dumdumbich.interview.webapiclient.domain.entity.User
 import com.dumdumbich.interview.webapiclient.domain.usecase.DataStorageUsecase
-import java.util.*
 
 
 class RoomDataStorage(context: Context) : DataStorageUsecase {
 
-    private val userDao: UserDao
-    private val repositoryDao: RepositoryDao
-
-    init {
-        val db = Room.databaseBuilder(
-            context,
-            GithubDb::class.java,
-            "github.db"
-        ).build()
-        userDao = db.userDao()
-        repositoryDao = db.repositoryDao()
-    }
-
-
-    override fun saveUser(user: User) {
-        userDao.insert(userToEntity(user, UUID.randomUUID().toString()))
-    }
-
-    override fun isUserInStorage(login: String): Boolean {
+    override fun getUser(
+        login: String,
+        onSuccess: (User) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
         TODO("Not yet implemented")
     }
 
-    override fun loadUser(login: String): User =
-        userFromEntity(userDao.get(login))
-
-    override fun saveAllUsers(users: List<User>) {
-        for (user in users) {
-            saveUser(user)
-        }
-    }
-
-    override fun readAllUsers(): List<User> {
-        val entities = userDao.getAll()
-        val users = emptyList<User>().toMutableList()
-        for (entity in entities) {
-            users.add(userFromEntity(entity))
-        }
-        return users
-    }
-
-    override fun deleteAllUsers() {
-        userDao.deleteAll()
-    }
-
-    override fun saveRepository(repository: Repository) {
-        repositoryDao.insert(repositoryToEntity(repository, UUID.randomUUID().toString()))
-    }
-
-    override fun isRepositoryInStorage(repositoryName: String, ownerLogin: String): Boolean {
+    override fun getUsers(
+        onSuccess: (List<User>) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
         TODO("Not yet implemented")
     }
 
-    override fun loadRepository(repositoryName: String, ownerLogin: String): Repository =
-        repositoryFromEntity(repositoryDao.get(repositoryName, ownerLogin))
-
-    override fun saveAllRepositories(repositories: List<Repository>) {
-        for (repository in repositories) {
-            saveRepository(repository)
-        }
+    override fun getRepository(
+        repositoryName: String,
+        userLogin: String,
+        onSuccess: (Repository) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
+        TODO("Not yet implemented")
     }
 
-    override fun readAllRepositories(): List<Repository> {
-        val entities = repositoryDao.getAll()
-        val repositories = emptyList<Repository>().toMutableList()
-        for (entity in entities) {
-            repositories.add(repositoryFromEntity(entity))
-        }
-        return repositories
+    override fun getRepositories(
+        userLogin: String,
+        onSuccess: (List<Repository>) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
+        TODO("Not yet implemented")
     }
 
-    override fun deleteAllRepositories() {
-        repositoryDao.deleteAll()
+    override fun putUser(
+        user: User,
+        onSuccess: (Int) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
+        TODO("Not yet implemented")
     }
 
-    private fun userToEntity(user: User, tableId: String): UserEntity =
-        UserEntity(
-            user.avatarUrl,
-            user.login,
-            user.reposUrl,
-            user.id,
-            tableId
-        )
+    override fun putUsers(
+        users: List<User>,
+        onSuccess: (Int) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
 
-    private fun userFromEntity(user: UserEntity): User =
-        User(
-            user.avatarUrl,
-            user.login,
-            user.reposUrl,
-            user.id
-        )
+    override fun putRepository(
+        repository: Repository,
+        onSuccess: (Int) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
 
-    private fun repositoryToEntity(repository: Repository, tableId: String): RepositoryEntity =
-        RepositoryEntity(
-            repository.name,
-            repository.ownerLogin,
-            repository.pushedAt,
-            repository.forks,
-            repository.id,
-            tableId
-        )
-
-    private fun repositoryFromEntity(repository: RepositoryEntity): Repository =
-        Repository(
-            repository.name,
-            repository.ownerLogin,
-            repository.pushedAt,
-            repository.forks,
-            repository.id,
-        )
+    override fun putRepositories(
+        repositories: List<Repository>,
+        onSuccess: (Int) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
 
 }
